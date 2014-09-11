58c500fbf1b2a96e218f00cc44995e6b1f2d9e90
tony <172993974@qq.com>
2014-09-11 14:31:44 +0800

    测试专题

--- javascript/js-test/mocha/add.spec.js
+++ javascript/js-test/mocha/add.spec.js
@@ -1,8 +1,11 @@
-var add = require('./app.js');
+var add = require('./add.js');
 var expect = require('chai').expect;
 
 describe('加法函数的测试', function () {
     it('1加1应该等于2', function () {
         expect(add(1, 1)).to.be.equal(2);
+    });
+    it('任何数加0应该等于自身', function () {
+        expect(add(1, 0)).to.be.equal(1);
     });
 });
