690c8c6018e283582cd8693d35215aaace8378f9
tony <172993974@qq.com>
2014-09-15 17:56:56 +0800

    第一个脚手架

--- nodejs/cli/command/init.js
+++ nodejs/cli/command/init.js
@@ -1,3 +1,40 @@
 'use strict'
 const exec = require('child_process').exec
 const co = require('co')
+const prompt = require('co-prompt')
+const config = require('../templates.json')
+const chalk = require('chalk')
+
+// 参考资料：https://juejin.im/post/5c237d1a5188256b9e0f21e1
+
+module.exports = () => {
+    co(function* () {
+        // 处理用户输入
+        let tplName = yield prompt('Template name：')
+        let projectName = yield prompt('Project name：')
+        let gitUrl
+        let branch
+
+        if (!config.tpl[tplName]) {
+            console.log(chalk.red('\n x Template does not exit!'))
+            process.exit()
+        }
+        gitUrl = config.tpl[tplName].url
+        branch = config.tpl[tplName].branch
+
+        // git命令，远程拉取项目并自定义项目名
+        let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`
+
+        console.log(chalk.white('\n Start generating...'))
+
+        exec(cmdStr, (error, stdout, stderr) => {
+            if (error) {
+                console.log(error)
+                process.exit()
+            }
+            console.log(chalk.green('\n √ Generation completed!'))
+            console.log(`\n cd ${projectName} && npm install \n`)
+            process.exit()
+        })
+    })
+}
