073d25811ab6ff761dd7252050ffd917b0b6a4dc
tony <172993974@qq.com>
2014-12-27 16:17:52 +0800

    webpack 配置模板

--- fetool/webpack/template/src/webpack-plugin-copy.js
+++ fetool/webpack/template/src/webpack-plugin-copy.js
@@ -1,1 +1,15 @@
-const fs = require('fs-extra')
+const fs = require("fs-extra");
+const globby = require("globby");
+class CopyDirWebpackPlugin {
+    constructor(options) {
+        this.options = options;
+    }
+    apply(compiler) {
+        const opt = this.options;
+        compiler.plugin('done', (stats) => {
+            if (process.env.NODE_ENV === 'production') {
+
+            }
+        })
+    }
+}
