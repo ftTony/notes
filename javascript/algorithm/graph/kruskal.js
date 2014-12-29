fb040eec2fa116428b105ba868054b3c3b2bc8e6
tony <172993974@qq.com>
2014-12-29 18:12:27 +0800

    图

--- javascript/algorithm/graph/kruskal.js
+++ javascript/algorithm/graph/kruskal.js
@@ -7,10 +7,28 @@ const find = (i, parent) => {
     return i
 }
 const union = (i, j, parent) => {
-
+    if (i !== j) {
+        parent[j] = i
+        return true
+    }
+    return false
 }
 const initializeCost = graph => {
-
+    const cost = []
+    const {
+        length
+    } = graph
+    for (let i = 0; i < length; i++) {
+        cost[i] = []
+        for (let j = 0; j < length; j++) {
+            if (graph[i][j] === 0) {
+                cost[i][j] = INF
+            } else {
+                cost[i][j] = graph[i][j]
+            }
+        }
+    }
+    return cost
 }
 const kruskal = graph => {
     const {
@@ -23,4 +41,22 @@ const kruskal = graph => {
     let u;
     let v;
     const cost = initializeColor(graph)
+    while (ne < length - 1) {
+        for (let i = 0, min = INF; i < length; i++) {
+            for (let j = 0; j < length; j++) {
+                if (cost[i][j] < min) {
+                    min = cost[i][j]
+                    a = u = i
+                    b = v = j
+                }
+            }
+        }
+        v = find(u, parent)
+        v = find(v, parent)
+        if (union(u, v, parent)) {
+            ne++
+        }
+        cost[a][b] = cost[b][a] = INF
+    }
+    return parent
 }
