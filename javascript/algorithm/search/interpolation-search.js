b49db8d62f5d4c96500ecf0c325257824f603a2c
tony <172993974@qq.com>
2015-01-18 14:38:45 +0800

    内插搜索

--- javascript/algorithm/search/interpolation-search.js
+++ javascript/algorithm/search/interpolation-search.js
@@ -17,7 +17,16 @@ export function interpolationSearch(array, value, compareFn = defaultCompare, eq
     let position = -1
     let delta = -1
     while (low <= high && biggerEquals(value, array[low], compareFn) && lesserEquals(value, array[high], compareFn)) {
-
+        delta = diffFn(value, array[low]) / diffFn(array[high], array[low])
+        position = low + Math.floor((high - low) * delta)
+        if (equalsFn(array[position], value)) {
+            return position
+        }
+        if (compareFn(array[position], value) === Compare.LESS_THAN) {
+            low = position + 1
+        } else {
+            high = position - 1
+        }
     }
     return DOES_NOT_EXIST
 }
