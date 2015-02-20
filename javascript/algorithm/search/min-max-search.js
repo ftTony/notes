8c8336e921d8f38fc4e010fef969e7f644825a7b
tony <172993974@qq.com>
2015-02-20 14:54:28 +0800

    搜索

--- javascript/algorithm/search/min-max-search.js
+++ javascript/algorithm/search/min-max-search.js
@@ -1,11 +1,30 @@
 import {
-    defaultCompare
+    defaultCompare,
+    Compare
 } from "../util";
 
 export function findMaxValue(array, compareFn = defaultCompare) {
-
+    if (array && array.length > 0) {
+        let max = array[0]
+        for (let i = 1; i < array.length; i++) {
+            if (compareFn(max, array[i]) === Compare.LESS_THAN) {
+                max = array[i]
+            }
+        }
+        return max
+    }
+    return undefined
 }
 
 export function findMaxValue(array, compareFn = defaultCompare) {
-
+    if (array && array.length > 0) {
+        let min = array[0]
+        for (let i = 1; i < array.length; i++) {
+            if (compareFn(min, array[i]) === Compare.BIGGER_THAN) {
+                min = array[i]
+            }
+        }
+        return min
+    }
+    return undefined
 }
