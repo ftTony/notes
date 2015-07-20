cf72cb2c3cc633ed3d829710cd9578c13e01eaf8
tony <172993974@qq.com>
2015-07-20 21:54:42 +0800

    广度遍历

--- javascript/algorithm/graph/breadth-first-search.js
+++ javascript/algorithm/graph/breadth-first-search.js
@@ -48,4 +48,31 @@ const BFS = (graph, startVertex) => {
     const vertices = graph.getVertices();
     const adjList = graph.getAdjList();
     const color = inittializeColor(vertices);
+    const queue = new Queue()
+    const distances = {}
+    const predecessors = {}
+    queue.enqueue(startVertex)
+    for (let i = 0; i < vertices.length; i++) {
+        distances[vertices[i]] = 0
+        predecessors[vertices[i]] = null
+    }
+    while (!queue.isEmpty()) {
+        const u = queue.dequeue()
+        const neighbors = adjList.get(u)
+        color[u] = Colors.GREY
+        for (let i = 0; i < neighbors.length; i++) {
+            const w = neighbors[i]
+            if (color[w] === Colors.WHITE) {
+                color[w] = Colors.GREY
+                distances[w] = distances[u] + 1
+                predecessors[w] = u
+                queue.enqueue(w)
+            }
+        }
+        color[u] = Colors.BLACK
+    }
+    return {
+        distances,
+        predecessors
+    }
 }
