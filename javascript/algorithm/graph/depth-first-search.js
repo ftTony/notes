ab4c1439ffc08dcc78a766d13834dcc8f25cccd1
tony <172993974@qq.com>
2015-07-22 22:24:54 +0800

    深度优先遍历

--- javascript/algorithm/graph/depth-first-search.js
+++ javascript/algorithm/graph/depth-first-search.js
@@ -6,3 +6,42 @@ const Colors = {
     GREY: 1,
     BLACK: 2
 }
+
+const initializeColor = vertices => {
+    const color = {}
+    for (let i = 0; i < vertices.length; i++) {
+        color[vertices[i]] = Colors.WHITE
+    }
+    return color
+}
+
+const depthFirstSearchVisit = (u, color, adjList, callback) => {
+    color[u] = Colors.GREY
+    if (callback) {
+        callback(u)
+    }
+    const neighbors = adjList.get(u)
+}
+
+const depthFirstSearch = (gragh, callback) => {
+
+};
+
+const DFSVisit = (v, color, d, f, p, time, adjList) => {
+
+};
+
+const DFS = gragh => {
+    const vertices = gragh.getVertices()
+    const adjList = gragh.getAdjList()
+    const color = initializeColor(vertices)
+    const d = {}
+    const f = {}
+    const p = {}
+    const time = {
+        count: 0
+    }
+    for (let i = 0; i < vertices.length; i++) {
+
+    }
+}
