019b28fd5afbbe4eff5eda284e1631afb3a18475
tony <172993974@qq.com>
2015-07-28 13:35:11 +0800

    深度遍历

--- javascript/algorithm/graph/depth-first-search.js
+++ javascript/algorithm/graph/depth-first-search.js
@@ -21,14 +21,37 @@ const depthFirstSearchVisit = (u, color, adjList, callback) => {
         callback(u)
     }
     const neighbors = adjList.get(u)
+    for (let i = 0; i < neighbors.length; i++) {
+        depthFirstSearchVisit(w, color, adjList, callback)
+    }
+    color[u] = Color.BLACK
 }
 
 const depthFirstSearch = (gragh, callback) => {
+    const vertices = gragh.getVertices()
+    const adjList = gragh.getAdjList()
+    const color = initializeColor(vertices)
 
+    for (let i = 0; i < vertices.length; i++) {
+        if (color[vertices[i]] === Colors.WHITE) {
+            depthFirstSearchVisit(vertices[i], color, adjList, callback)
+        }
+    }
 };
 
 const DFSVisit = (v, color, d, f, p, time, adjList) => {
-
+    color[u] = Colors.GREY
+    d[u] = ++time.count
+    const neighbors = adjList.get(u)
+    for (let i = 0; i < neighbors.length; i++) {
+        const w = neighbors[i]
+        if (color[w] === Colors.WHITE) {
+            p[w] = u
+            DFSVisit(w, color, d, f, p, time, adjList)
+        }
+    }
+    color[u] = Colors.BLACK
+    f[u] = ++time.count
 };
 
 const DFS = gragh => {
@@ -42,6 +65,19 @@ const DFS = gragh => {
         count: 0
     }
     for (let i = 0; i < vertices.length; i++) {
+        f[vertices[i]] = 0
+        d[vertices[i]] = 0
+        p[vertices[i]] = null
 
+    }
+    for (let i = 0; i < vertices.length; i++) {
+        if (color[vertices[i]] === Colors.WHITE) {
+            DFSVisit(vertices[i], color, d, f, p, time, adjList);
+        }
+    }
+    return {
+        discovery: d,
+        finished: f,
+        predecessors: p
     }
 }
