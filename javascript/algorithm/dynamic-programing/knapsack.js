c694b535b94333f2e0537aca2c0105c09774c52b
tony <172993974@qq.com>
2015-10-10 22:31:34 +0800

    添加相关算法

--- javascript/algorithm/dynamic-programing/knapsack.js
+++ javascript/algorithm/dynamic-programing/knapsack.js
@@ -1,5 +1,11 @@
 function findValues(n, capacity, kS) {
+    let i = n;
+    let k = capacity;
+    while (i > 0 && k > 0) {
+        if (kS[i][k] !== kS[i - 1][k]) {
 
+        }
+    }
 }
 
 export function knapSack(capacity, weights, values, n) {
---
+++ javascript/algorithm/greedy/knapsack.js
@@ -0,0 +1,3 @@
+function knapSack(capacity, weights, values) {
+
+}
