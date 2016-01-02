d7e7cb8579b30c1e47b79507672ee47940b02ab9
tony <172993974@qq.com>
2016-01-02 18:35:08 +0800

    红黑树

--- javascript/data-structures/red-black-tree-es6.js
+++ javascript/data-structures/red-black-tree-es6.js
@@ -1,12 +1,70 @@
+const Compare = {
+    LESS_THAN: -1,
+    BIGGER_THAN: 1,
+    EQUALS: 0
+}
+
+function defaultCompare(a, b) {
+    if (a === b) {
+        return Compare.EQUALS
+    }
+    return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+class Node {
+    constructor(key) {
+        this.key = key
+        this.left = undefined
+        this.right = undefined
+    }
+    toString() {
+        return `${this.key}`
+    }
+}
+
 class RedBlackNode extends Node {
     constructor(key) {
         super(key)
         this.key = key
         this.color = Colors.RED
-        this.parent = null
     }
     isRed() {
         return this.color === Colors.RED
+    }
+    flipColor() {
+        if (this.color === Colors.RED) {
+            this.color = Colors.BLACK
+        } else {
+            this.color = Colors.RED
+        }
+    }
+}
+
+const Colors = {
+    RED: 0,
+    BLACK: 1
+}
+
+import BinarySearchTree from './birary-search-tree-es6'
+
+class RedBlackTree extends BinarySearchTree {
+    constructor(compareFn = defaultCompare) {
+        super(compareFn)
+    }
+    rotationLL(node) {
+
+    }
+    rotationRR(node) {
+
+    }
+    insert(key) {
+        if (this.root == null) {
+            this.root = new RedBlackNode(key)
+            this.root.color = Colors.BLACK
+        } else {
+            const newNode = this.insertNode(this.root, key)
+            this.fixTreeProperties(newNode)
+        }
     }
     insertNode(node, key) {
         if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
@@ -25,12 +83,10 @@ class RedBlackNode extends Node {
             return this.insertNode(node.right, key)
         }
     }
-}
+    fixTreeProperties(node) {
 
-class RedBlackTree extends BinarySearchTree {
-    constructor(compareFn = defaultCompare) {
-        super(compareFn)
-        this.compareFn = compareFn
-        this.root = null
+    }
+    getRoot() {
+
     }
 }
