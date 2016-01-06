6967ae0ba0a731fa99673f10fd2a92d145eb17d1
tony <172993974@qq.com>
2016-01-06 16:32:31 +0800

    heap

--- javascript/data-structures/heap-es6.js
+++ javascript/data-structures/heap-es6.js
@@ -75,13 +75,33 @@ class MinHeap {
     }
     siftUp(index) {
         let parent = this.getParentIndex(index)
-        while (index > 0 && this.compareFn(this.heap[parent], this.heap[index]) === Compare.BIGGER_THAN)
+        while (index > 0 && this.compareFn(this.heap[parent], this.heap[index]) === Compare.BIGGER_THAN) {
+            swap(this.heap, parent, index)
+            index = parent
+            parent = this.getParentIndex(index)
+        }
     }
     extract() {
-
+        if (this.isEmpty()) {
+            return undefined
+        }
+        if (this.size() === 1) {
+            return this.heap.shift()
+        }
+        const removeValue = this.heap[0]
+        this.heap[0] = this.heap.pop()
+        this.siftDown(0)
+        return removeValue
     }
     heapify(array) {
-
+        if (array) {
+            this.heap = array
+        }
+        const maxIndex = Math.floor(this.size() / 2) - 1
+        for (let i = 0; i <= maxIndex; i++) {
+            this.siftDown(i)
+        }
+        return this.heap
     }
     getAsArray() {
         return this.heap
@@ -90,6 +110,8 @@ class MinHeap {
 
 class Max extends MinHeap {
     constructor(compareFn = defaultCompare) {
-
+        super(compareFn)
+        this.compareFn = compareFn
+        this.compareFn = reverseCompare(compareFn)
     }
 }
