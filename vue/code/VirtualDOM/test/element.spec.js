0a1c6bebc928f7492a57c73379146e2f02fc7560
tony <172993974@qq.com>
2016-03-19 17:35:39 +0800

    测试用例

---
+++ vue/code/VirtualDOM/test/element.spec.js
@@ -0,0 +1,5 @@
+var el = require('../lib/element')
+
+var chai = require('chai')
+var sinon = require('sinon')
+var sinionChai = require('sinon-chai')
---
+++ vue/code/VirtualDOM/test/patch.spec.js
@@ -0,0 +1,8 @@
+var el = require('../lib/element')
+var diff = require('../lib/diff')
+var patch = require('../lib/patch')
+var jsdom = require('mocha-jsdom')
+
+var chai = require('chai')
+var sinon = require('sinon')
+var sinonChai = require('sinon-chai')
