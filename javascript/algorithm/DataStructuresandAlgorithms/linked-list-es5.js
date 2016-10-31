2d03238f8663a0f26f2ca22e67e7260096e53cba
tony <172993974@qq.com>
2016-10-31 11:49:25 +0800

    '增加链表'

--- javascript/algorithm/DataStructuresandAlgorithms/linked-list-es5.js
+++ javascript/algorithm/DataStructuresandAlgorithms/linked-list-es5.js
@@ -1,56 +1,57 @@
-/* 
+/** 
 单链表
+*/
 
 function Node(element) {
-    this.element = element;
-    this.next = null;
+  this.element = element
+  this.next = null
 }
 
 function LList() {
-    this.head = new Node('head');
-    this.find = find;
-    this.insert = insert;
-    this.remove = remove;
-    this.display = display;
+  this.head = new Node('head')
+  this.find = find
+  this.insert = insert
+  this.remove = remove
+  this.display = display
 }
 
 function find(item) {
-    var currNode = this.head;
-    while (currNode.element != item) {
-        currNode = currNode.next;
-    }
-    return currNode;
+  var currNode = this.head
+  while (currNode.element != item) {
+    currNode = currNode.next
+  }
+  return currNode
 }
 
 function insert(newElement, item) {
-    var newNode = new Node(newElement);
-    var current = this.find(item);
-    newNode.next = current.next;
-    current.next = newNode;
+  var newNode = new Node(newElement)
+  var current = this.find(item)
+  newNode.next = current.next
+  current.next = newNode
 }
 
 function display() {
-    var currNode = this.head;
-    while (!(currNode.next == null)) {
-        console.log(currNode.next.element);
-        currNode = currNode.next;
-    }
+  var currNode = this.head
+  while (!(currNode.next == null)) {
+    console.log(currNode.next.element)
+    currNode = currNode.next
+  }
 }
 
 function findPrevious(item) {
-    var currNode = this.head;
-    while (!(currNode.next == null) && (currNode.next.element != item)) {
-        currNode = currNode.next;
-    }
-    return currNode;
+  var currNode = this.head
+  while (!(currNode.next == null) && currNode.next.element != item) {
+    currNode = currNode.next
+  }
+  return currNode
 }
 
-function remove(item){
-    var prevNode=this.findPrevious(item);
-    if(!(prevNode.next==null)){
-        prevNode.next=prevNode.next.next;
-    }
-} */
+function remove(item) {
+  var prevNode = this.findPrevious(item)
+  if (!(prevNode.next == null)) {
+    prevNode.next = prevNode.next.next
+  }
+}
 
 /* function Node(element) {
     this.element = element;
