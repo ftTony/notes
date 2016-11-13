7355ac1baa8cdc585db0c9fc3482cbcb6a1daf31
tony <172993974@qq.com>
2016-11-13 17:06:08 +0800

    'add'

---
+++ javascript/data-structures/stack-linked-list-es6.js
@@ -0,0 +1,33 @@
+import DoublyLinkList from './doubly-linked-list-es6'
+
+class StackLinkedList {
+  constructor() {
+      this.items = new DoublyLinkList();
+    }
+    push (element) {
+        this.items.push(element){
+
+        }
+    }
+    pop () {
+        
+    }
+    peek () {
+        
+    }
+    isEmpty () {
+        
+    }
+    size () {
+        
+    }
+    size () {
+        
+    }
+    clear () {
+        
+    }
+    toString () {
+        return this.items.toString();
+    }
+}
