ef2bf10f5887a105d75d9e5f4a4ac336be2504f2
tony <172993974@qq.com>
2016-11-19 11:05:46 +0800

    'set-es6'

--- javascript/data-structures/set-es6.js
+++ javascript/data-structures/set-es6.js
@@ -5,12 +5,32 @@ class Set {
   constructor() {
     this.items = {}
   }
-  add(element) {}
-  delete(element) {}
-  has(element) {}
-  values() {}
+  add(element) {
+    if (!this.has(element)) {
+      this.items[element] = element
+      return true
+    }
+    return false
+  }
+  delete(element) {
+    if (this.has(element)) {
+      delete this.items[element]
+      return true
+    }
+    return false
+  }
+  has(element) {
+    return Object.prototype.hasOwnProperty.call(this.items, element)
+  }
+  values() {
+    return Object.values(this.items)
+  }
   union(otherSet) {}
   intersection(otherSet) {}
   difference(otherSet) {}
   isSubsetOf(otherSet) {}
+  isEmpty() {}
+  size() {}
+  clear() {}
+  toString() {}
 }
