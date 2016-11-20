57b53b915d2c30f5b104a7e3b51ad074bde3991a
tony <172993974@qq.com>
2016-11-20 11:24:17 +0800

    'set-es6'

--- javascript/data-structures/set-es6.js
+++ javascript/data-structures/set-es6.js
@@ -25,12 +25,68 @@ class Set {
   values() {
     return Object.values(this.items)
   }
-  union(otherSet) {}
-  intersection(otherSet) {}
-  difference(otherSet) {}
-  isSubsetOf(otherSet) {}
-  isEmpty() {}
-  size() {}
-  clear() {}
-  toString() {}
+  union(otherSet) {
+    const unionSet = new Set()
+    this.values().forEach(value => unionSet.add(value))
+    otherSet.values().forEach(value => unionSet.add(value))
+    return unionSet
+  }
+  intersection(otherSet) {
+    const intersectionSet = new Set()
+    const values = this.values()
+    const otherValues = otherSet.values()
+    let biggerSet = values
+    let smallerSet = otherValues
+    if (otherValues.length - values.length > 0) {
+    }
+    smallerSet.forEach(value => {
+      if (biggerSet.includes(value)) {
+        intersectionSet.add(value)
+      }
+    })
+    return intersectionSet
+  }
+  difference(otherSet) {
+    const differenceSet = new Set()
+    this.values().forEach(value => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value)
+      }
+    })
+    return differenceSet
+  }
+  isSubsetOf(otherSet) {
+    if (this.size() > otherSet.size()) {
+      return false
+    }
+    let isSubset = true
+    this.values().every(value => {
+      if (!otherSet.has(value)) {
+        isSubset = false
+        return false
+      }
+      return true
+    })
+    return isSubsetOf
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  size() {
+    return Object.keys(this.items).length
+  }
+  clear() {
+    this.items = {}
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return ''
+    }
+    const values = this.values()
+    let objString = `${values[0]}`
+    for (let i = 1; i < values.length; i++) {
+      objString = `${objString},${values[i].toString()}`
+    }
+    return objString
+  }
 }
