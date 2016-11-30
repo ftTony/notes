e839de56509c27eabfa1c6f7d4ec072a1b304ddd
tony <172993974@qq.com>
2016-11-30 17:04:58 +0800

    '散列表'

--- javascript/data-structures/hash-table-es6.js
+++ javascript/data-structures/hash-table-es6.js
@@ -8,6 +8,16 @@ function defaultToString(item) {
   }
   return item.toString()
 }
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key
+    this.value = value
+  }
+  toString() {
+    return `[#${this.key}:${this.value}]`
+  }
+}
 class HashTable {
   constructor(toStrFn = defaultToString) {
     this.toStrFn = toStrFn
@@ -22,25 +32,55 @@ class HashTable {
     for (let i = 0; i < tableKey.length; i++) {
       hash += tableKey.charCodeAt(i)
     }
+    return hash % 37
   }
   hashCode(key) {
     return this.loseloseHashCode(key)
   }
   put(key, value) {
     if (key != null && value != null) {
+      const position = this.hashCode(key)
+      this.table[position] = new ValuePair(key, value)
+      return true
     }
+    return false
   }
-  get(key) {}
-  remove(key) {}
+  get(key) {
+    const valuePair = this.table[this.hashCode(key)]
+    return valuePair == null ? undefined : valuePair.value
+  }
+  remove(key) {
+    const hash = this.hashCode(key)
+    const valuePair = this.table[hash]
+    if (valuePair != null) {
+      delete this.table[hash]
+      return true
+    }
+    return false
+  }
   getTable() {
     return this.table
   }
   isEmpty() {
     return this.size() === 0
   }
-  size() {}
+  size() {
+    return Object.keys(this.table).length
+  }
   clear() {
     this.table = {}
   }
-  toString() {}
+  toString() {
+    if (this.isEmpty()) {
+      return ''
+    }
+    const keys = Object.keys(this.table)
+    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`
+    for (let i = 1; i < keys.length; i++) {
+      objString = `${objString},{${keys[i]} => ${this.table[
+        keys[i]
+      ].toString()}}`
+    }
+    return objString
+  }
 }
