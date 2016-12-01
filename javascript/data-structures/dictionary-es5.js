81296154ec411cd1228b663eb9da475b2470bd78
tony <172993974@qq.com>
2016-12-01 17:16:43 +0800

    '散列表'

--- javascript/data-structures/dictionary-es5.js
+++ javascript/data-structures/dictionary-es5.js
@@ -1,52 +1,49 @@
 function Dictionary() {
-    var items = {};
+  var items = {}
 
-    this.has = function (key) {
-        return key in items;
+  this.has = function(key) {
+    return key in items
+  }
+
+  this.set = function(key, value) {
+    items[key] = value
+  }
+
+  this.remove = function(key) {
+    if (this.has(key)) {
+      delete items[key]
+
+      return true
     }
+    return false
+  }
 
-    this.set = function (key, value) {
-        items[key] = value;
+  this.get = function(key) {
+    return this.has(key) ? items[key] : undefined
+  }
+
+  this.values = function() {
+    var values = {}
+    for (var k in items) {
+      if (this.has(k)) {
+        values.push(items[k])
+      }
     }
+    return values
+  }
 
-    this.remove = function (key) {
-        if (this.has(key)) {
-            delete items[key];
+  this.getItems = function() {
+    return items
+  }
 
-            return true;
-        }
-        return false;
-    }
+  this.size = function() {
+    return Object.keys(items).length
+  }
 
-    this.get = function (key) {
-        return this.has(key) ? items[key] : undefined;
-    }
-
-    this.values = function (key) {
-        var values = {};
-        for (var k in items) {
-            if (this.has(k)) {
-                values.push(items[k]);
-            }
-        }
-        return values;
-    }
-
-    this.getItems = function () {
-        return items;
-    }
-}
-
-function count() {
-    var n = 0;
-    for (var key in Object.keys(this.datastore)) {
-        ++n;
-    }
-    return n;
-}
-
-function clear() {
-    for (var key in Object.keys(this.datastore)) {
-        delete this.datastore[key];
-    }
+  this.keys = function() {
+    return Object.keys(items)
+  }
+  this.clear = function() {
+    items = {}
+  }
 }
