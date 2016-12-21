64f62905c51f8a0c907c7f373dbec6f48fb4872b
tony <172993974@qq.com>
2016-12-21 10:48:51 +0800

    二叉搜索树

--- javascript/data-structures/birary-search-tree-es6.js
+++ javascript/data-structures/birary-search-tree-es6.js
@@ -54,42 +54,102 @@ class BinarySearchTree {
         return this.searchNode(this.root, key)
     }
     searchNode(node, key) {
-
+        if (node == null) {
+            return false
+        }
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            return this.searchNode(node.left, key)
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            return this.searchNode(node.right, key)
+        }
+        return true
     }
+    // 中序遍历
     inOrderTraverse(callback) {
         this.inOrderTraveseNode(this.root, callback)
     }
     inOrderTraveseNode(node, callback) {
-
+        if (node != null) {
+            this.inOrderTraveseNode(node.left, callback)
+            callback(node.key)
+            this.inOrderTraveseNode(node.right, callback)
+        }
     }
+    // 前序遍历
     preOrderTravese(callback) {
         this.preOrderTraveseNode(this.root, callback)
     }
     preOrderTraveseNode(node, callback) {
-
+        if (node != null) {
+            callback(node.key)
+            this.preOrderTraveseNode(node.left, callback)
+            this.preOrderTraveseNode(node.right, callback)
+        }
     }
     postOrderTravese(callback) {
         this.postOrderTraveseNode(this.root, callback)
     }
     postOrderTraveseNode(node, callback) {
-
+        if (node != null) {
+            this.postOrderTraveseNode(node.left, callback);
+            this.postOrderTraveseNode(node.right, callback);
+            callback(node.key)
+        }
     }
+    // 寻找最小值
     min() {
         return this.minNode(this.root)
     }
     minNode(node) {
-
+        let current = ndoe
+        while (current != null && current.left != null) {
+            current = current.left;
+        }
+        return current
     }
+    // 寻找最大值
     max() {
         return this.maxNode(this.root)
     }
     maxNode(node) {
-
+        let current = node;
+        while (current != null && current.right != null) {
+            current = current.right
+        }
+        return current
     }
+    // 移除元素
     remove(key) {
         this.root = this.removeNode(this.root, key)
     }
     removeNode(node, key) {
+        if (node === null) {
+            return undefined
+        }
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            node.left = this.removeNode(node.left, key)
+            return node
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            node.right = this.removeNode(node.right, key)
+            return node
+        }
 
+        if (node.left == null && node.right == null) {
+            node = undefined
+            return node
+        }
+
+        if (node.left == null) {
+            node = node.right
+            return node
+        } else if (node.right == null) {
+            node = node.left
+            return node
+        }
+
+        const aux = this.minNode(node.right)
+        node.key = aux.key
+        node.right = this.removeNode(node.right, aux.key)
+        return node
     }
 }
