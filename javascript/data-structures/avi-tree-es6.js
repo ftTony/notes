f6b17c3c2fde65a6743b5850de68a424a2bfdf2e
tony <172993974@qq.com>
2016-12-22 11:17:13 +0800

    avl树

--- javascript/data-structures/avi-tree-es6.js
+++ javascript/data-structures/avi-tree-es6.js
@@ -42,15 +42,53 @@ class AVITree extends BinarySearchTree {
         if (node === null) {
             return -1
         }
+        return Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) + 1
     }
+    /**
+     * Left left case: rotate right
+     *
+     *       b                           a
+     *      / \                         / \
+     *     a   e -> rotationLL(b) ->   c   b
+     *    / \                             / \
+     *   c   d                           d   e
+     *
+     * @param node Node<T>
+     */
     rotationLL(node) {
+        const tmp = node.left
+        node.left = tmp.right
+        tmp.right = node
+        return tmp
+    }
+    /**
+     * Right right case: rotate left
+     *
+     *     a                              b
+     *    / \                            / \
+     *   c   b   -> rotationRR(a) ->    a   e
+     *      / \                        / \
+     *     d   e                      c   d
+     *
+     * @param node Node<T>
+     */
+    rotationRR(node) {
 
     }
+    /**
+     * Left right case: rotate left then right
+     * @param node Node<T>
+     */
     rotationLR(node) {
 
     }
+    /**
+     * Right left case: rotate right then left
+     * @param node Node<T>
+     */
     rotationRL(node) {
-
+        node.right = this.rotationLL(node.right)
+        return this.rotationRR(node)
     }
     getBalanceFactor(node) {
 
