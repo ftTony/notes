dbbac9ac6798998556d5b275c5e64d3acefbc48c
tony <172993974@qq.com>
2016-12-30 20:31:43 +0800

    红黑树

---
+++ javascript/data-structures/red-black-tree-es6.js
@@ -0,0 +1,36 @@
+class RedBlackNode extends Node {
+    constructor(key) {
+        super(key)
+        this.key = key
+        this.color = Colors.RED
+        this.parent = null
+    }
+    isRed() {
+        return this.color === Colors.RED
+    }
+    insertNode(node, key) {
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            if (node.left == null) {
+                node.left = new RedBlackNode(key)
+                node.left.parent = node
+                return node.left
+            } else {
+                return this.insertNode(node.left, key)
+            }
+        } else if (node.right == null) {
+            node.right = new RedBlackNode(key)
+            node.right.parent = node
+            return node.right
+        } else {
+            return this.insertNode(node.right, key)
+        }
+    }
+}
+
+class RedBlackTree extends BinarySearchTree {
+    constructor(compareFn = defaultCompare) {
+        super(compareFn)
+        this.compareFn = compareFn
+        this.root = null
+    }
+}
