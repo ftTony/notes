000ae1c3ada51999c7c903e2243c0a0ebef1865e
tony <172993974@qq.com>
2017-03-06 16:50:43 +0800

    '路由'

--- vue/Exercise/router/01/index.js
+++ vue/Exercise/router/01/index.js
@@ -1,2 +1,35 @@
-import { BaseRouter } from './base';
-export class HistoryRouter extends BaseRouter {}
+import { HashRouter } from './hash';
+import { HistoryRouter } from './history';
+import { ROUTELIST } from './routeList';
+
+// 路由模式
+const MODE = 'hash';
+class WebRouter {
+  constructor({ mode = 'hash', routeList }) {
+    this.router =
+      mode === 'hash'
+        ? new HashRouter(routeList)
+        : new HistoryRouter(routeList);
+  }
+  push(path) {
+    this.router.push(path);
+  }
+  replace(path) {
+    this.router.replace(path);
+  }
+  go(num) {
+    this.router.go(num);
+  }
+}
+
+document.querySelector('.btn-list').addEventListener('click', e => {
+  const event = e || window.event;
+  if (event.target.tagName === 'LI') {
+    const url = event.target.dataset.url;
+    !url.indexOf('/') ? webRouter.push(url) : webRouter.go(url);
+  }
+});
+
+document.querySelector('.replace-btn').addEventListener('click', e => {
+  WebRouter.replace('/');
+});
