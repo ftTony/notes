f3ec8ddcbb23ddd08c0454e687a769af463333cb
tony <172993974@qq.com>
2019-01-11 14:16:37 +0800

    '例子'

---
+++ fetool/webpack/07/utils.js
@@ -0,0 +1,8 @@
+export function square (x) {
+    return x * x;
+}
+
+// // 增加 cube() 求立方函数
+// export function cube (x) {
+//     return x * x * x;
+// }
---
+++ fetool/webpack/07/webpack.config.js
@@ -0,0 +1,28 @@
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    entry: {
+        index: './index.js',
+        utils: './utils.js'
+    },
+    output: {
+        filename: '[name].[chunkhash].js',   // 输出index.js和utils.js
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // 用 MiniCssExtractPlugin 抽离出css文件，以link标签的形式引入样式文件
+        new MiniCssExtractPlugin({
+            filename: 'index.[contenthash].css'        // 输出的css文件名为index.css
+        })
+    ]
+}
