0654eed28198b4e0c81c385c011d48e55dfb3af3
tony <tony@futunn.com>
2019-02-14 17:42:11 +0800

    柯里化完善

---  InterviewQuestion/curry.js
+++  InterviewQuestion/curry.js
@@ -1,19 +1,53 @@
-function sub_curry(fn) {
-    var args = [].slice.call(arguments, 1);
-    return function () {
-        return fn.apply(this, args.concat([].slice.call(arguments)));
-    }
-}
+/**
+ * 第一版
+ */
 
-function curry(fn, length) {
-    length = length || fn.length
-    var slice = Array.prototype.slice;
-    return function () {
-        if (arguments.length < length) {
-            var combined = [fn].concat(slice.call(arguments));
-            return curry(sub_curry.apply(this, combined), length - arguments.length);
-        } else {
-            return fn.apply(this, arguments);
-        }
-    }
-}
+// function sub_curry(fn) {
+//     var args = [].slice.call(arguments, 1);
+//     return function () {
+//         return fn.apply(this, args.concat([].slice.call(arguments)));
+//     }
+// }
+
+// function curry(fn, length) {
+//     length = length || fn.length
+//     var slice = Array.prototype.slice;
+//     return function () {
+//         if (arguments.length < length) {
+//             var combined = [fn].concat(slice.call(arguments));
+//             return curry(sub_curry.apply(this, combined), length - arguments.length);
+//         } else {
+//             return fn.apply(this, arguments);
+//         }
+//     }
+// }
+
+// 第二版
+
+// function curry(fn, args) {
+//     var length = fn.length;
+//     args = args || [];
+    
+//     return function(){
+//         var _args=args.slice(0),
+//                _args=_args.concat([].slice.call(arguments));
+//             if(_args.length<length){
+//                 return curry.call(this,fn,_args);
+//             }else{
+//                 return fn.apply(this,_args);
+//             }
+//     }
+// }
+
+// var fn=curry(function(a,b,c){
+//     console.log([a,b,c]);
+// });
+
+// fn("a", "b", "c") // ["a", "b", "c"]
+// fn("a", "b")("c") // ["a", "b", "c"]
+// fn("a")("b")("c") // ["a", "b", "c"]
+// fn("a")("b", "c") // ["a", "b", "c"]
+
+//es6 写法
+
+var curry=fn=>judge=(...args)=>args.length===fn.length?fn(...args):(arg)=>judge(...args,arg)
