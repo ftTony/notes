fdcaf60d794ae6260cd6a99a6ee0a180470f7d6a
xiaonengwu <xiaonengwu@kemai.cn>
2019-05-05 18:00:32 +0800

    '研究element源码'

---
+++ vue/Exercise/element/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <el-button @click="click">测试弹窗</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    click () {
+      this.$alert('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${action}`
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
