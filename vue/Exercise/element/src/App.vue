6996236bbb811dc381441fe6f4c03a4d891ed5af
tony <tony@futunn.com>
2019-06-16 21:33:02 +0800

    element anasly

--- vue/Exercise/element/src/App.vue
+++ vue/Exercise/element/src/App.vue
@@ -1,12 +1,52 @@
 <template>
   <div id="app">
     <el-button @click="click">测试弹窗</el-button>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    <el-select
+      v-model="value"
+      placeholder="请选择"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data(){
+      return {
+        options: [{
+                value: '选项1',
+                label: '黄金糕'
+              }, {
+                value: '选项2',
+                label: '双皮奶'
+              }, {
+                value: '选项3',
+                label: '蚵仔煎'
+              }, {
+                value: '选项4',
+                label: '龙须面'
+              }, {
+                value: '选项5',
+                label: '北京烤鸭'
+              }],
+              value: ''
+      }
+  },
   methods: {
     click () {
       this.$alert('这是一段内容', '标题名称', {
