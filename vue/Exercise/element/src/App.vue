8ec72644ee9be190477e9bb82f907ac1cdaa3a1e
tony <xiaonengwu@kemai.cn>
2019-06-20 10:15:29 +0800

    'element分析'

--- vue/Exercise/element/src/App.vue
+++ vue/Exercise/element/src/App.vue
@@ -10,6 +10,7 @@
     </el-container>
     <el-select
       v-model="value"
+      multiple
       placeholder="请选择"
     >
       <el-option
@@ -26,26 +27,26 @@
 <script>
 export default {
   name: 'App',
-  data(){
-      return {
-        options: [{
-                value: '选项1',
-                label: '黄金糕'
-              }, {
-                value: '选项2',
-                label: '双皮奶'
-              }, {
-                value: '选项3',
-                label: '蚵仔煎'
-              }, {
-                value: '选项4',
-                label: '龙须面'
-              }, {
-                value: '选项5',
-                label: '北京烤鸭'
-              }],
-              value: ''
-      }
+  data () {
+    return {
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
+    }
   },
   methods: {
     click () {
