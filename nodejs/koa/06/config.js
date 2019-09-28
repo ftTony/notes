6287450b252aff24d046150c5e279b1e9f9c45e3
tony <xiaonengwu@kemai.cn>
2019-09-28 17:18:25 +0800

    'koa实践'

---
+++ nodejs/koa/06/config.js
@@ -0,0 +1,12 @@
+const config = {
+    port: 3001,
+    database: {
+        DATABASE: 'koa_demo',
+        USERNAME: 'root',
+        PASSWORD: '1990yu101',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
