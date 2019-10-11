94b9bf4005016ec5ed536a15a17e730fcc07e2b4
tony <172993974@qq.com>
2019-10-11 16:55:21 +0800

    'koa完善前端代码'

---
+++ nodejs/koa/06/static/src/apps/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import HeadeNav from './../components/header-nav.jsx'
+import FooterCommon from './../components/footer-common.jsx'
+
+class App extends React.Component {
+
+}
+
+export default App
--- nodejs/koa/06/static/src/pages/admin.js
+++ nodejs/koa/06/static/src/pages/admin.js
@@ -3,3 +3,5 @@ import 'whatwg-fetch'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './../apps/admin.jsx'
+
+ReactDOM.render(<App />, document.getElementById('app'))
