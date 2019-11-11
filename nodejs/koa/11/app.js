0bfce33cdacfd0bdfe2752b3dbb1376763fee1f8
tony <172993974@qq.com>
2019-11-11 17:50:22 +0800

    'koa聊天'

--- nodejs/koa/11/app.js
+++ nodejs/koa/11/app.js
@@ -1,30 +1,30 @@
-const Koa = require('koa')
-const app = new Koa()
+var Koa = require('koa');
+var app = new Koa();
 const Router = require('koa-router');
 const fs = require('fs');
-const server = require('http').createServer(app.callback())
-const io = require('socket.io')(server)
-const port = 8081
-
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server);
 
 // 首页路由
 let router = new Router();
 router.get('/', ctx => {
     ctx.response.type = 'html';
     ctx.response.body = fs.createReadStream('./index.html');
-})
-
+});
 app.use(router.routes());
 
-io.on('connection', socket => {
-    console.log('socket初始始化完成');
-    socket.on('say', data => {
-        console.log(data, '接收到的信息')
-        socket.emit('message', { hell: '你是谁' })
-    })
-})
+// socket连接
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
-
-app.listen(process.env.PORT || port, () => {
-    console.log(`app run at : http://127.0.0.1:${port}`);
+// 监听端口
+server.listen(8081, () => {
+    console.log('listening on *:8081');
 });
