08aa7aff99afc418f43c2aa4c755853ef21735c0
tony <172993974@qq.com>
2019-11-27 10:51:19 +0800

    'typescript'

--- typescript/01/index.js
+++ typescript/01/index.js
@@ -11,3 +11,72 @@ var myAge = 25;
 // 模板字符串
 var sentence = "Hello,my name is " + myName + ",I'll be " + (myAge +
     1) + " years old next month.";
+function alertName() {
+    console.log('My name is tony');
+}
+var unusabel = undefined;
+var myFavoriteNumber = 'seven';
+myFavoriteNumber = 7;
+var anyThing = 'Tom';
+anyThing.setName('Jerry');
+anyThing.setName('Jerry').sayHello();
+anyThing.myName.setFirstName('Cat');
+var myNumber;
+myNumber = 'seven';
+myNumber = 7;
+function getString(something) {
+    return something.toString();
+}
+var tom = {
+    id: 89757,
+    name: 'Tom',
+    age: 25,
+    gender: 'male'
+};
+var fibonacci = [1, 1, 2, 3, 5];
+var nacci = [1, 1, 2, 3, 5];
+// 函数声明
+function sum(x, y) {
+    return x + y;
+}
+// 函数表达式
+var mySum = function (x, y) {
+    return x + y;
+};
+var mySearch;
+mySearch = function (source, subString) {
+    return source.search(subString) !== -1;
+};
+// 参数默认值
+function buildName(firstName, lastName) {
+    if (lastName === void 0) { lastName = 'Cat'; }
+    return firstName + ' ' + lastName;
+}
+var tomcat = buildName('Tom', 'Cat');
+var tony = buildName('Tony');
+function push(array) {
+    var items = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        items[_i - 1] = arguments[_i];
+    }
+    items.forEach(function (item) {
+        array.push(item);
+    });
+}
+var ab = [];
+push(ab, 1, 2, 3);
+function reverse(x) {
+    if (typeof x === 'number') {
+        return Number(x
+            .toString()
+            .split('')
+            .reverse()
+            .join(''));
+    }
+    else if (typeof x === 'string') {
+        return x
+            .split('')
+            .reverse()
+            .join('');
+    }
+}
--- typescript/01/index.ts
+++ typescript/01/index.ts
@@ -18,3 +18,105 @@ let myAge: number = 25
 
 let sentence: string = `Hello,my name is ${myName},I'll be ${myAge +
   1} years old next month.`
+
+function alertName(): void {
+  console.log('My name is tony')
+}
+
+let unusabel: void = undefined
+
+let myFavoriteNumber: any = 'seven'
+
+myFavoriteNumber = 7
+
+let anyThing: any = 'Tom'
+anyThing.setName('Jerry')
+anyThing.setName('Jerry').sayHello()
+anyThing.myName.setFirstName('Cat')
+
+let myNumber: string | number
+myNumber = 'seven'
+myNumber = 7
+
+function getString(something: string | number): string {
+  return something.toString()
+}
+
+interface Person {
+  readonly id: number
+  name: string
+  age: number
+  [propName: string]: any
+}
+
+let tom: Person = {
+  id: 89757,
+  name: 'Tom',
+  age: 25,
+  gender: 'male'
+}
+
+let fibonacci: number[] = [1, 1, 2, 3, 5]
+
+interface NumberArray {
+  [index: number]: number
+}
+
+let nacci: NumberArray = [1, 1, 2, 3, 5]
+
+// 函数声明
+function sum(x: number, y: number): number {
+  return x + y
+}
+
+// 函数表达式
+let mySum: (x: number, y: number) => number = function(
+  x: number,
+  y: number
+): number {
+  return x + y
+}
+
+// 用接口定义函数的形状
+interface SearchFunc {
+  (source: string, subString: string): boolean
+}
+let mySearch: SearchFunc
+mySearch = function(source: string, subString: string) {
+  return source.search(subString) !== -1
+}
+
+// 参数默认值
+function buildName(firstName: string, lastName: string = 'Cat') {
+  return firstName + ' ' + lastName
+}
+let tomcat = buildName('Tom', 'Cat')
+let tony = buildName('Tony')
+
+function push(array: any[], ...items: any[]) {
+  items.forEach(function(item) {
+    array.push(item)
+  })
+}
+
+let ab: any[] = []
+push(ab, 1, 2, 3)
+
+function reverse(x: number): number
+function reverse(x: string): string
+function reverse(x: number | string): number | string {
+  if (typeof x === 'number') {
+    return Number(
+      x
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    )
+  } else if (typeof x === 'string') {
+    return x
+      .split('')
+      .reverse()
+      .join('')
+  }
+}
