78aefde94d15f5210a698a8f87c198cc450db5d6
tony <172993974@qq.com>
2020-03-08 08:35:08 +0800

    双端队列

--- javascript/data-structures/circular-linked-list-es6.js
+++ javascript/data-structures/circular-linked-list-es6.js
@@ -1,38 +1,38 @@
 /**
  * 循环链表
  */
-CircularLinkedList extends LinkedList{
-    constructor(equalsFn = ((a,b)=>a===b)){
+CircularLinkedList extends LinkedList {
+    constructor(equalsFn = ((a, b) => a === b)) {
         super(equalsFn);
     }
-    push(element){
+    push(element) {
         const node = new Node(element);
         let current;
-        if(this.head == null){
+        if (this.head == null) {
             this.head = node;
-        }else{
+        } else {
             current = this.getElementAt(this.size() - 1);
             current.next = node;
         }
         node.next = this.head;
         this.count++;
     }
-    insert(element,index){
-        if(index>=0 && index <= this.count){
+    insert(element, index) {
+        if (index >= 0 && index <= this.count) {
             const node = new Node(element);
             let current = this.head;
-            if(index === 0){
-                if(this.head == null){
+            if (index === 0) {
+                if (this.head == null) {
                     this.head = node;
                     node.next = this.head;
-                }else{
+                } else {
                     node.next = current;
                     current = this.getElementAt(this.size());
                     this.head = node;
                     current.next = this.head;
                 }
-            }else{
-                const previous = this.getElementAt(index-1);
+            } else {
+                const previous = this.getElementAt(index - 1);
                 node.next = previous.next;
                 previous.next = node;
             }
@@ -41,17 +41,17 @@ CircularLinkedList extends LinkedList{
         }
         return false;
     }
-    removeAt(index){
-        if(index>=0 && index<this.count){
+    removeAt(index) {
+        if (index >= 0 && index < this.count) {
             let current = this.head;
-            if(index === 0){
-                if(this.size() === 1){
+            if (index === 0) {
+                if (this.size() === 1) {
                     this.head = undefined;
-                }else{
+                } else {
 
                 }
-            }else{
-                const previous = this.getElementAt(index-1);
+            } else {
+                const previous = this.getElementAt(index - 1);
                 current = previous.next;
                 previous.next = current.next;
             }
