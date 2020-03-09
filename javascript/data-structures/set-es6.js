151c3b37b503dfc58dcb23fdb28f6ebfce34b9b3
tony <172993974@qq.com>
2020-03-09 11:00:37 +0800

    '集合'

--- javascript/data-structures/set-es6.js
+++ javascript/data-structures/set-es6.js
@@ -2,13 +2,15 @@
  * es6 set
  */
 class Set {
-    constructor() {
-        this.items = {};
-    }
-    add(element) {
-
-    }
-    delete(element) {
-
-    }
+  constructor() {
+    this.items = {}
+  }
+  add(element) {}
+  delete(element) {}
+  has(element) {}
+  values() {}
+  union(otherSet) {}
+  intersection(otherSet) {}
+  difference(otherSet) {}
+  isSubsetOf(otherSet) {}
 }
