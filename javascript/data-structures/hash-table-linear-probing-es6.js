ed63cbf6e24ad297514b5d17405668d5e6a9c318
tony <172993974@qq.com>
2020-03-13 16:48:09 +0800

    线性探查

--- javascript/data-structures/hash-table-linear-probing-es6.js
+++ javascript/data-structures/hash-table-linear-probing-es6.js
@@ -8,6 +8,15 @@ function defaultToString(item) {
     }
     return item.toString()
 }
+class ValuePair {
+    constructor(key, value) {
+        this.key = key
+        this.value = value
+    }
+    toString() {
+        return `[#${this.key}:${this.value}]`
+    }
+}
 class HashTableLinearProbing {
     constructor(toStrFn = defaultToString) {
         this.toStrFn = toStrFn
@@ -28,13 +37,67 @@ class HashTableLinearProbing {
         return this.loseloseHashCode(key)
     }
     put(key, value) {
-
+        if (key != null && value != null) {
+            const position = this.hashCode(key)
+            if (this.table[position] == null) {
+                this.table[position] = new ValuePair(key, value)
+            } else {
+                let index = position + 1
+                while (this.table[index] != null) {
+                    index++
+                }
+                this.table[index] = new ValuePair(key, value)
+            }
+            return true
+        }
+        return false
     }
     get(key) {
-
+        const position = this.hashCode(key)
+        if (this.table[position] != null) {
+            if (this.table[position].key === key) {
+                return this.table[position].value
+            }
+            let index = position + 1
+            while (this.table[index] != null && this.table[index].key !== key) {
+                index++
+            }
+            if (this.table[index] != null && this.table[index].key !== key) {
+                return this.table[position].value
+            }
+        }
+        return undefined
+    }
+    remove(key) {
+        const position = this.hashCode(key)
+        if (this.table[position] != null) {
+            if (this.table[position].key === key) {
+                delete this.table[position]
+                this.verifyRemoveSideEffect(key, position)
+                return true
+            }
+            let index = position + 1
+            while (this.table[index] != null && this.table[index].key !== key) {
+                index++
+            }
+            if (this.table[index] != null && this.table[index].key === key) {
+                delete this.table[index]
+                this.verifyRemoveSideEffect(key, index)
+                return true
+            }
+        }
+        return false
     }
     verifyRemoveSideEffect(key, removedPosition) {
+        const hash = this.hashCode(key)
+        let index = removedPosition + 1
+        while (this.table[index] != null) {
+            const posHash = this.hashCode(this.table[index].key)
+            if (posHash <= hash || posHash <= removedPosition) {
 
+            }
+            index++;
+        }
     }
     isEmpty() {
         return this.size() === 0
