19a980a7ea28261f0f3e8c2f5aedd5019c786d9f
tony <172993974@qq.com>
2020-03-14 08:24:12 +0800

    二叉搜索树

---
+++ javascript/data-structures/birary-search-tree-es6.js
@@ -0,0 +1,56 @@
+class BinarySearchTree {
+    contructor() {
+
+    }
+    insert(key) {
+
+    }
+    insertNode(node, key) {
+
+    }
+    getRoot() {
+        return this.root
+    }
+    search(key) {
+
+    }
+    searchNode(node, key) {
+
+    }
+    inOrderTraverse(callback) {
+
+    }
+    inOrderTraveseNode(node, callback) {
+
+    }
+    preOrderTravese(callback) {
+
+    }
+    preOrderTraveseNode(node, callback) {
+
+    }
+    postOrderTravese(callback) {
+
+    }
+    postOrderTraveseNode(node, callback) {
+
+    }
+    min() {
+        return this.minNode(this.root)
+    }
+    minNode(node) {
+
+    }
+    max() {
+        return this.maxNode(this.root)
+    }
+    maxNode(node) {
+
+    }
+    remove(key) {
+
+    }
+    removeNode(node, key) {
+
+    }
+}
