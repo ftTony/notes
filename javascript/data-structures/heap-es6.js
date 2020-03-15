ed3129b341645e74ebbaaeb5398fb8ad62e86fa0
tony <172993974@qq.com>
2020-03-15 16:16:06 +0800

    二叉堆

--- javascript/data-structures/heap-es6.js
+++ javascript/data-structures/heap-es6.js
@@ -21,37 +21,61 @@ function swap(array, a, b) {
 
 class MinHeap {
     constructor(compareFn = defaultCompare) {
-
+        this.compareFn = compareFn
+        this.heap = []
     }
     getLeftIndex(index) {
-
+        return (2 * index) + 1
     }
     getRightIndex(index) {
-
+        return (2 * index) + 2
     }
     getParentIndex(index) {
-
+        if (index === 0) {
+            return undefined
+        }
+        return Math.floor((index - 1) / 2)
     }
     size() {
-
+        return this.heap.length
     }
     isEmpty() {
-
+        return this.size() <= 0
     }
     clear() {
         this.heap = []
     }
     findMinimum() {
-
+        return this.isEmpty() ? undefined : this.heap[0]
     }
     insert(value) {
-
+        if (value != null) {
+            const index = this.heap.length
+            this.heap.push(value)
+            this.siftUp(index)
+            return true
+        }
+        return false
     }
     siftDown(index) {
-
+        let element = index
+        const left = this.getLeftIndex(index)
+        const right = this.getRightIndex(index)
+        const size = this.size()
+        if (left < size && this.compareFn(this.heap[element], this.heap[left]) === Compare.BIGGER_THAN) {
+            element = left
+        }
+        if (right < size && this.compareFn(this.heap[element], this.heap[right]) === Compare.BIGGER_THAN) {
+            element = right
+        }
+        if (index !== element) {
+            swap(this.heap, index, element)
+            this.siftDown(element)
+        }
     }
     siftUp(index) {
-
+        let parent = this.getParentIndex(index)
+        while (index > 0 && this.compareFn(this.heap[parent], this.heap[index]) === Compare.BIGGER_THAN)
     }
     extract() {
 
