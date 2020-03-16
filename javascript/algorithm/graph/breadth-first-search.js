ecd5a8946d1715efb42b84b4c3fe514161f639b5
tony <172993974@qq.com>
2020-03-16 14:54:40 +0800

    广度遍历

--- javascript/algorithm/graph/breadth-first-search.js
+++ javascript/algorithm/graph/breadth-first-search.js
@@ -1,3 +1,23 @@
 /**
  * 广度遍历
  */
+
+import Queue from '../../data-structures/queue-es6'
+
+const Colors = {
+    WHITE: 0,
+    GREY: 1,
+    BLACK: 2
+}
+
+const inittializeColor = vertices => {
+
+}
+
+const breadthFristSearch = (graph, startVertex, callback) => {
+    const vertices = graph.getVertices()
+}
+
+const BFS = (graph, startVertex) => {
+
+}
