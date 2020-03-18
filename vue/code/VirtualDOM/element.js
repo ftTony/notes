44552f547412fabdb1eca9d4a4a5ec231bf0396c
tony <172993974@qq.com>
2020-03-18 08:42:21 +0800

    DDOM实现

--- vue/code/VirtualDOM/element.js
+++ vue/code/VirtualDOM/element.js
@@ -1,17 +1,27 @@
 /**
+ * 参考 https://github.com/livoras/simple-virtual-dom
  * Element virdual-dom对象定义
  * @param {String} tagName - dom 元素名称
  * @param {Object} props - dom 属性
  * @param {Arrray<Element|String>} - 子节点
  */
 
+var _ = require('./util')
+
 function Element(tagName, props, children) {
     this.tagName = tagName
     this.props = props
     this.children = children
-    
+
 }
 
 Element.prototype.render = function () {
+    var el = document.createElement(this.tagName)
+    var props = this.props
 
+    for (var propName in props) {
+
+    }
 }
+
+module.exports = Element
---
+++ vue/code/VirtualDOM/util.js
@@ -0,0 +1,33 @@
+var _ = exports
+
+_.type = function (obj) {
+    return Object.prototype.toString.call(obj).replace(/\[object\s|\]/g, '')
+}
+
+_.isArray = function isArray(list) {
+
+}
+
+_.slice = function slice(arrayLike, index) {
+
+}
+
+_.truthy = function truthy(value) {
+
+}
+
+_.isString = function isString(list) {
+
+}
+
+_.each = function each(array, fn) {
+
+}
+
+_.toArray = function toArray(listLike) {
+
+}
+
+_.setAttr = function setAttr(node, key, value) {
+
+}
