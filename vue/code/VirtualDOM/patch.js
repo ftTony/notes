f1bf91a8dbadc1b957a2f68bbe9e7e1a8592e24c
tony <172993974@qq.com>
2020-03-18 16:23:36 +0800

    patch方法

--- vue/code/VirtualDOM/patch.js
+++ vue/code/VirtualDOM/patch.js
@@ -42,9 +42,9 @@ function applyPatches(node, currentPatches) {
                 break
             case TEXT:
                 if (node.textContent) {
-
+                    node.textContent = currentPatch.content
                 } else {
-
+                    node.nodeValue = currentPatch.content
                 }
                 break
             default:
@@ -68,11 +68,26 @@ function reorderChildren(node, moves) {
     var staticNodeList = _.toArray(node.childNodes)
     var maps = {}
     _.each(staticNodeList, function (node) {
-
+        if (node.nodeType === 1) {
+            var key = node.getAttribute('key')
+            if (key) {
+                maps[key] = node
+            }
+        }
     })
 
     _.each(moves, function (move) {
         var index = move.index
+        if (move.type === 0) {
+            if (staticNodeList[index] === node.childNodes[index]) {
+                node.removeChild(node.childNodes[index])
+            }
+            staticNodeList.splice(index, 1)
+        } else if (move.type === 1) {
+            var insertNode = maps[move.item.key] ? maps[move.item.key].cloneNode(true) : (typeof move.item === 'object') ? move.item.render() : document.createTextNode(move.item)
+            staticNodeList.splice(index, 0, insertNode)
+            node.insertBefore(insertNode, node.childNodes[index] || null)
+        }
     })
 }
 
