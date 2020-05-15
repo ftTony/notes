abb3ec8a9b9d2495907bec238014fbeb9b3a84a7
tony <172993974@qq.com>
2020-05-15 21:59:42 +0800

    webpack配置模板

--- fetool/webpack/template/build/build.js
+++ fetool/webpack/template/build/build.js
@@ -12,5 +12,20 @@ const spinner = ora('开始构建项目...')
 spinner.start()
 
 webpack(config.toConfig(), function (err, stats) {
+    spinner.stop()
+    if (err) throw err
+    process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+    }) + '\n\n')
 
+    if (stats.hasErrors()) {
+        console.log(chalk.red('构建失败\n'))
+        process.exit(1)
+    }
+
+    console.log(chalk.cyan('build完成\n'))
 })
--- fetool/webpack/template/build/dev.js
+++ fetool/webpack/template/build/dev.js
@@ -6,3 +6,19 @@ const port = 8080
 const publicPath = '/common/'
 
 config.devServer.quiet(true).hot(true).https(false).disableHostCheck(true).publicPath(publicPath).clientLogLevel('none')
+
+const compilper = webpack(config.toConfig())
+// 拿到devServer参数
+const chainDevServer = compiler.options.devServer
+const server = new WebpackDevServer(compiler, Object.assign(chainDevServer, {}))
+
+['SIGINT', 'SIGTERM'].forEach(signal => {
+    process.on(signal, () => {
+        server.close(() => {
+            process.exit(0)
+        })
+    })
+})
+
+// 监听端口
+server.listen(port)
