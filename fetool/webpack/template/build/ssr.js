51a371f94a718a4443d5163efc02c84820d5f183
tony <172993974@qq.com>
2020-05-17 15:55:55 +0800

    ssr

--- fetool/webpack/template/build/ssr.js
+++ fetool/webpack/template/build/ssr.js
@@ -1,6 +1,27 @@
 module.exports = function (options) {
-    const path = require('path')
-    const Config = require('webpack-chain')
-    const config = new Config()
-    const webpack = require('webpack')
-}
+    const path = require("path");
+    const Config = require("webpack-chain");
+    const config = new Config();
+    const webpack = require("webpack");
+    const rimraf = require("rimraf");
+    const ora = require("ora");
+    const chalk = require("chalk");
+    const PATHS = {
+        build: path.join(process.cwd(), "static"),
+        ssrDemo: path.join(process.cwd(), 'src', 'ssr.jsx')
+    };
+
+    require('../config/babelLoader')({
+        config,
+        tsx: true
+    })()
+    require('../config/HtmlWebpackPlugin')({
+        config,
+        options: {
+            publicPath: '/',
+            filename: 'client.ssr.html'
+        }
+    })()
+
+    config.entry('ssr')
+};
