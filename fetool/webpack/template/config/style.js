b1d6029cf341b7d18b7b9c6fa479831a457ba0ec
tony <172993974@qq.com>
2020-05-17 12:09:15 +0800

    安装依赖包

--- fetool/webpack/template/config/style.js
+++ fetool/webpack/template/config/style.js
@@ -2,19 +2,13 @@ module.exports = (config, resolve) => {
     return (lang, test) => {
         const baseRule = config.module.rule(lang).test(test);
         const normalRule = baseRule.oneOf('normal');
-        applyLoaders(normalRule)
 
-        function applyLoaders(rule) {
-            rule
-                .use('extract-css-loader')
-                .loader(require("mini-css-extract-plugin").loader)
-                .options({
-                    publicPath: './'
-                })
-            rule
-                .use('css-loader')
-                .loader('css-loader')
-                .options({})
-        }
+        normalRule.use('extract-css-loader').loader(require('mini-css-extract-plugin').loader).options({
+            hmr: process.env.NODE_ENV === 'development',
+            publicPath: '/'
+        })
+        normalRule.use('css-loader').loader(require.resolve('css-loader')).options({})
+
+        normalRule.use('postcss-loader').loader('')
     }
 }
