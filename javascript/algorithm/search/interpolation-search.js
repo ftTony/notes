68e65734689869f7ca902a9b552f133460e5970f
tony <172993974@qq.com>
2020-05-19 10:20:25 +0800

    搜索算法

---
+++ javascript/algorithm/search/interpolation-search.js
@@ -0,0 +1,13 @@
+import {
+    biggerEquals,
+    Compare,
+    defaultCompare,
+    defaultEquals,
+    defaultDiff,
+    DOES_NOT_EXIST,
+    lesserEquals
+} from '../util'
+
+export function interpolationSearch(array, value) {
+
+}
--- javascript/algorithm/util.js
+++ javascript/algorithm/util.js
@@ -2,13 +2,33 @@ export const Compare = {
     LESS_THAN: -1,
     BIGGER_THAN: 1,
     EQUALS: 0
-};
+}
+
+export const DOES_NOT_EXIST = -1
+
+export function defaultEquals(a, b) {
+    return a === b
+}
 
 export function defaultCompare(a, b) {
     if (a === b) {
         return Compare.EQUALS
     }
     return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+export function lesserEquals(a, b, compareFn) {
+    const comp = compareFn(a, b)
+    return comp === Compare.LESS_THAN || comp === Compare.EQUALS
+}
+
+export function defaultDiff() {
+    return Number(a) - Number(b)
+}
+
+export function biggerEquals(a, b, compareFn) {
+    const comp = compareFn(a, b)
+    return comp === Compare.BIGGER_THAN || comp === Compare.EQUALS
 }
 
 export function swap(array, a, b) {
