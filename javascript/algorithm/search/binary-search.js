4638b51a452bb2efd46d719bbf326ff8d066c601
tony <172993974@qq.com>
2020-05-23 18:30:24 +0800

    二分查找

--- javascript/algorithm/search/binary-search.js
+++ javascript/algorithm/search/binary-search.js
@@ -1,3 +1,18 @@
-export function knapSack(capacity, weights, values, n) {
+import {
+    Compare,
+    defaultCompare,
+    DOES_NOT_EXIST
+} from '../util'
 
+import {
+    quickSort2
+} from './sorting/quicksort'
+
+export function binarySearch(array, value, compareFn = defaultCompare) {
+    const sortedArray = quickSort2(array)
+    let low = 0
+    let high = sortedArray.length - 1
+    while (low <= high) {
+
+    }
 }
