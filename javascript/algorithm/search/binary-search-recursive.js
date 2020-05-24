a3cfbc33fec8070d06d3f1dbcd7dd599bd3eecf6
tony <172993974@qq.com>
2020-05-24 14:50:52 +0800

    递归二分摸查

--- javascript/algorithm/search/binary-search-recursive.js
+++ javascript/algorithm/search/binary-search-recursive.js
@@ -16,25 +16,3 @@ function binSearch1(target, arr, start, end) {
     }
     return -1;
 }
-
-
-/**
- * 方法二
- * @param {*} arr 
- * @param {*} data 
- */
-function binSearch2(arr, data) {
-    var upperBound = arr.length - 1;
-    var lowerBound = 0;
-    while (lowerBound <= upperBound) {
-        var mid = Math.floor((upperBound + lowerBound) / 2);
-        if (arr[mid] < data) {
-            lowerBound = mid + 1;
-        } else if (arr[mid] > data) {
-            upperBound = mid - 1;
-        } else {
-            return mid;
-        }
-    }
-    return -1;
-}
