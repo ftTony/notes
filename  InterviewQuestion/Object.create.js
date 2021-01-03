7e937454cb3afbf60ed7998846d78c98d94774bf
tony <172993974@qq.com>
2021-01-03 14:44:31 +0800

    Object.create

---  InterviewQuestion/Object.create.js
+++  InterviewQuestion/Object.create.js
@@ -1,3 +1,12 @@
 /**
  * Object.create()方法创建一个新对象，使用现有的对象来提供新创建的对象的__proto__。
  */
+Object.create2 = function(proto,propertyObject = undefined){
+    if(typeof proto !== 'object' && typeof proto !=='function'){
+        throw new TypeError('Object prototype may only be an Object or null');
+    }
+    if(propertyObject == null){
+        new TypeError('Cannot convert undefined or null to object');
+    }
+    function F(){}
+}
