9071da0cf00d38fdd16be1163008d1d2811aaaff
tony <172993974@qq.com>
2021-01-06 14:56:16 +0800

    Object.create

---  InterviewQuestion/Object.assign.js
+++  InterviewQuestion/Object.assign.js
@@ -1,3 +1,14 @@
 Object.assign2 = function(target,...source){
-    
+    if(target == null){
+        throw new TypeError('Cannot convert undefined or null to object');
+    }
+    let ret = Object(target);
+    source.forEach(function(obj){
+        if(obj !=null){
+            for(let key in obj){
+                ret[key] = obj[key];
+            }            
+        }
+    })
+    return ret;
 }
---  InterviewQuestion/Object.create.js
+++  InterviewQuestion/Object.create.js
@@ -9,4 +9,14 @@ Object.create2 = function(proto,propertyObject = undefined){
         new TypeError('Cannot convert undefined or null to object');
     }
     function F(){}
+    F.prototype = proto;
+    const obj = new F();
+    if(propertyObject != undefined){
+        Object.defineProperties(obj,propertyObject);
+    }
+    if(proto === null){
+        // 创建一个没有原型对象的对象，Object.create(null)
+        obj.__proto__ = null;        
+    }
+    return obj;
 }
