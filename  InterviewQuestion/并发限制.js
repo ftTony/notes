50aa0465a76e235704c49476b1723c92b6432fe3
tony <172993974@qq.com>
2021-02-27 16:00:02 +0800

    promiseall

---  InterviewQuestion/并发限制.js
+++  InterviewQuestion/并发限制.js
@@ -12,5 +12,18 @@ function asyncPool(poolLimit,array,iteratorFn){
         const item = array[i++];
         const p = Promise.resolve().then(()=>iteratorFn(item,array));
         // 放入promise数组
-    }
+        ret.push(p);
+        // promise执行完毕，从executing数组中删除
+        const e = p.then(() =>executing.splice(executing.indexOf(e),1));
+        // 插入executing数字，表示正在执行的promise
+        executing.push(e);
+        // 使用promise.rece，每当executing数组中promise数量低于poolLimit，就实例化新的promise并执行
+        let r = Promise.resolve();
+        if(executing.length >=poolLimit){
+            r = Promise.race(executing);
+        }
+        // 递归，直到遍历完array
+        return r.then(()=>enqueue());
+    };
+    return enqueue().then(()=>Promise.all(ret));
 }
