dbe77911ba501a64dd60b22d138e96f00799892d
tony <172993974@qq.com>
2021-03-30 09:26:09 +0800

    手写webpack

--- fetool/webpack/12/index.js
+++ fetool/webpack/12/index.js
@@ -2,14 +2,34 @@
 const config = require('./minipack.config');
 // 入口
 const entry = config.entry;
-const content = fs.readFile(entry,'utf-8');
 
 const babelParser = require('@babel/parser');
-const ast = babelParser.parse(content,{
-    sourceType:"module"
-});
+
+const traverse = require('@babel/traverse').default;
 
 const {transformFromAst} = require('@babel/core');
-const {code} = transformFromAst(ast,null,{
-    presets:['@babel/preset-env'],
-})
+
+
+function createAsset(filename){
+    const content = fs.readFile(entry,'utf-8');
+    const ast = babelParser.parse(content,{
+        sourceType:"module"
+    });
+    const dependencies = [];
+    traverse(ast,{
+        ImportDeclaration:({node})=>{
+            dependencies.push(node.source.value)
+        }
+    });
+
+    // 获取文件内容
+    const {code} = transformFromAst(ast,null,{
+        presets:['@babel/preset-env'],
+    });
+    // 返回结果
+    return {
+        dependencies,
+        code
+    }
+}
+
