f3854b819ca060c803ac22180a71d110be2f59fa
tony <172993974@qq.com>
2021-04-11 10:16:43 +0800

    图片懒加载

---  InterviewQuestion/图片懒加载.js
+++  InterviewQuestion/图片懒加载.js
@@ -4,4 +4,28 @@
  * 
  * 图片全部加载完成后移除事件监听
  * 加载完的图片，从imgList移除
+ * https://zhuanlan.zhihu.com/p/55311726（原理）
  */
+
+let imgList = [...document.querySelectorAll('img')]
+let length = imgList.length;
+
+const imgLazyLoad = function(){
+    let count = 0;
+    return (function(){
+        let deleteIndexList = [];
+        imgList.forEach((img,index)=>{
+            let rect = img.getBoundingClientRect()
+            if(rect.top < window.innerHeight){
+                img.src = img.dataset.src;
+                deleteIndexList.push(index);
+                count++;
+                if(count === length){
+                    document.removeEventListener('scroll',imgList)
+                }
+            }
+        });
+        imgList = imgList.filter((img,index)=>!deleteIndexList.includes(index));
+    })()
+}
+document.addEventListener('scroll', imgLazyLoad);
