6b900e8bc19e4784de2269d03838920513f2a913
tony <172993974@qq.com>
2021-04-11 15:13:43 +0800

    偏函数

---  InterviewQuestion/partial.js
+++  InterviewQuestion/partial.js
@@ -8,7 +8,14 @@ var _ = {};
 
 function partial(fn){
     var args = [].slice.call(arguments,1);
-    
+    return function(){
+        var position = 0,len = args.length;
+        for(var i=0;i<len;i++){
+            args[i] = args[i] === _?arguments[position++]:args[i];
+        }
+        while(position<arguments.length) args.push(arguments[position++]);
+        return fn.apply(this,args);
+    }
 }
 
 // 第二版
