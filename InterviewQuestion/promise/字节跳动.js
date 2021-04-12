79936ba3f34c65bd2ef315068842e0da01f82b5a
tony <172993974@qq.com>
2021-04-12 12:00:33 +0800

    处理并发数

--- InterviewQuestion/promise/字节跳动.js
+++ InterviewQuestion/promise/字节跳动.js
@@ -1,7 +1,8 @@
 //JS实现一个带并发限制的异步调度器Scheduler，保证同时运行的任务最多有两个。完善代码中Scheduler类，使得以下程序能正确输出
 class Scheduler {
     add(promiseCreator) {
-
+      let list = [];
+      return Promise.all()
      }
     
   }
@@ -12,13 +13,12 @@ class Scheduler {
    
   const scheduler = new Scheduler()
   const addTask = (time, order) => {
-    scheduler.add(() => timeout(time))
-      .then(() => console.log(order))
+    scheduler.add(() => timeout(time)).then(() => console.log(order));
   }
    
   addTask(1000, '1')
   addTask(500, '2')
-  addTask(300, '3')
+  addTask(800, '3')
   addTask(400, '4')
   // output: 2 3 1 4
    
