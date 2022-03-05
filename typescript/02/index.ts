219c36e6f95e514b7a272a10ad583fa8482a4bd0
xiaonengwu@webull.com <172993974@qq.com>
2022-03-05 15:07:54 +0800

    动画学习

--- typescript/02/index.ts
+++ typescript/02/index.ts
@@ -1,2 +1,7 @@
-let message: string = 'Hello Web';
-console.log(message);
+// let message: string = 'Hello Web';
+// console.log(message);
+
+let myFavariteNumber:string | number;
+myFavariteNumber = "seven";
+console.log(myFavariteNumber);
+myFavariteNumber = 7;
